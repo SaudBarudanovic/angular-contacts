972dcb4fa79e265934451e08d331c16c1aa185fe
unknown <saud.barudanovic@gmail.com>
2020-05-15 17:42:25 +0200

    Added remote PostgreSQL database connection

--- backend/database/postgres.js
+++ backend/database/postgres.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "Redacted",
-    host: "localhost",
+    user: "xdhmsyvt",
+    password: "W47-l77XOkMk4ltXVLg95nhVaaqeK_OZ",
+    host: "rogue.db.elephantsql.com",
     port: 5432,
-    database: "ang_contacts"
+    database: "xdhmsyvt"
 })
 
 module.exports = pool;
